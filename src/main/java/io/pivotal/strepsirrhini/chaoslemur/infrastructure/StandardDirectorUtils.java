07bc828292e37e18bd20d7a4ac336ffe9a942f8d
poojsharma <poojsharma2stubhub.com>
2018-08-09 11:21:24 -0700

    fixed token refresh

--- src/main/java/io/pivotal/strepsirrhini/chaoslemur/infrastructure/StandardDirectorUtils.java
+++ src/main/java/io/pivotal/strepsirrhini/chaoslemur/infrastructure/StandardDirectorUtils.java
@@ -43,7 +43,6 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 import javax.net.ssl.SSLContext;
 import java.net.URI;
 import java.security.GeneralSecurityException;
@@ -57,119 +56,125 @@ import java.util.stream.Collectors;
 @ConditionalOnProperty("director.host")
 final class StandardDirectorUtils implements DirectorUtils {
 	private static final Logger log = LoggerFactory.getLogger(StandardDirectorUtils.class);
-	
-    private final RestTemplate restTemplate;
 
-    private final URI root;
+	private RestTemplate restTemplate;
 
-    @Autowired
-    StandardDirectorUtils(@Value("${director.host}") String host,
-                          @Value("${director.username}") String username,
-                          @Value("${director.password}") String password,
-                          Set<ClientHttpRequestInterceptor> interceptors) throws GeneralSecurityException {
-        this(createRestTemplate(host, username, password, interceptors), UriComponentsBuilder.newInstance().scheme("https").host(host).port(25555).build().toUri());
-    }
+	private final URI root;
 
-    StandardDirectorUtils(RestTemplate restTemplate, URI root) {
-        this.restTemplate = restTemplate;
-        this.root = root;
-    }
+	private String host;
+	private String username;
+	private String password;
+	private Set<ClientHttpRequestInterceptor> interceptors;
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public Set<String> getDeployments() {
-        URI deploymentsUri = UriComponentsBuilder.fromUri(this.root)
-            .path("deployments")
-            .build().toUri();
+	@Autowired
+	StandardDirectorUtils(@Value("${director.host}") String host, @Value("${director.username}") String username,
+			@Value("${director.password}") String password, Set<ClientHttpRequestInterceptor> interceptors)
+			throws GeneralSecurityException {
+		this(createRestTemplate(host, username, password, interceptors),
+				UriComponentsBuilder.newInstance().scheme("https").host(host).port(25555).build().toUri(), host,
+				username, password, interceptors);
 
-        List<Map<String, String>> deployments = this.restTemplate.getForObject(deploymentsUri, List.class);
+	}
 
-        return deployments.stream()
-            .map(deployment -> deployment.get("name"))
-            .collect(Collectors.toSet());
-    }
+	StandardDirectorUtils(RestTemplate restTemplate, URI root, String host, String username, String password,
+			Set<ClientHttpRequestInterceptor> interceptors) {
+		this.restTemplate = restTemplate;
+		this.root = root;
+		this.host = host;
+		this.username = username;
+		this.password = password;
+		this.interceptors = interceptors;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public Set<Map<String, String>> getVirtualMachines(String deployment) {
-        URI vmsUri = UriComponentsBuilder.fromUri(this.root)
-            .pathSegment("deployments", deployment, "vms")
-            .build().toUri();
+	@SuppressWarnings("unchecked")
+	@Override
+	public Set<String> getDeployments() {
+		URI deploymentsUri = UriComponentsBuilder.fromUri(this.root).path("deployments").build().toUri();
+		try {
+			this.restTemplate = createRestTemplate(host, username, password, interceptors);
+		} catch (GeneralSecurityException e) {
+			log.error("Failed to fetch all deployment info from BOSH", e.getMessage());
+		}
+		List<Map<String, String>> deployments = this.restTemplate.getForObject(deploymentsUri, List.class);
 
-        return this.restTemplate.getForObject(vmsUri, Set.class);
-    }
+		return deployments.stream().map(deployment -> deployment.get("name")).collect(Collectors.toSet());
+	}
 
-    private static RestTemplate createRestTemplate(String host, String username, String password, Set<ClientHttpRequestInterceptor> interceptors) throws GeneralSecurityException {
-        
-    	String directorUaaBearerToken = getBoshDirectorUaaToken(host,username,password);
-    	log.info("directorUaaBearerToken:" + directorUaaBearerToken);
-    	
-		SSLContext sslContext = SSLContexts.custom()
-	            .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-	            .useTLS()
-	            .build();
+	@SuppressWarnings("unchecked")
+	@Override
+	public Set<Map<String, String>> getVirtualMachines(String deployment) {
+		URI vmsUri = UriComponentsBuilder.fromUri(this.root).pathSegment("deployments", deployment, "vms").build()
+				.toUri();
+		try {
+			this.restTemplate = createRestTemplate(host, username, password, interceptors);
+		} catch (GeneralSecurityException e) {
+			log.error("Failed to fetch info for deployment:" + deployment + " from BOSH ", e.getMessage());
+		}
+		return this.restTemplate.getForObject(vmsUri, Set.class);
+	}
 
-	        SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, new AllowAllHostnameVerifier());
+	private static RestTemplate createRestTemplate(String host, String username, String password,
+			Set<ClientHttpRequestInterceptor> interceptors) throws GeneralSecurityException {
 
-	        Header auth = new BasicHeader("Authorization", String.format("Bearer %s", directorUaaBearerToken));
-	        Header accept = new BasicHeader("Accept", "*/*");
-	        List<Header> headers = new ArrayList<Header>();
-	        headers.add(auth);
-	        headers.add(accept);
-	        
+		String directorUaaBearerToken = getBoshDirectorUaaToken(host, username, password);
+		log.info("directorUaaBearerToken:" + directorUaaBearerToken);
 
-	        HttpClient httpClient = HttpClientBuilder.create()
-	            .disableRedirectHandling()
-	            .setDefaultHeaders(headers)
-	            .setSSLSocketFactory(connectionFactory)
-	            .build();
-        
-        
-        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
-        restTemplate.getInterceptors().addAll(interceptors);
+		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).useTLS()
+				.build();
 
-        return restTemplate;
-    }
-    
-    
-    private static String getBoshDirectorUaaToken(String host, String directorName, String password) throws  GeneralSecurityException{
-    	
-        SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-                .useTLS()
-                .build();
+		SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext,
+				new AllowAllHostnameVerifier());
 
-            SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, new AllowAllHostnameVerifier());
+		Header auth = new BasicHeader("Authorization", String.format("Bearer %s", directorUaaBearerToken));
+		Header accept = new BasicHeader("Accept", "*/*");
+		List<Header> headers = new ArrayList<Header>();
+		headers.add(auth);
+		headers.add(accept);
 
-            HttpClient httpClient = HttpClientBuilder.create()
-                .disableRedirectHandling()
-                .setSSLSocketFactory(connectionFactory)
-                .build();
-            RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
-            
-            MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-            String base64Passowrd = encodePassword(directorName,password);
-            log.info("base64Passowrd:" + base64Passowrd);
-            headers.add("Authorization", "Basic " + base64Passowrd);
-            headers.add("Content-Type", "application/x-www-form-urlencoded");
+		HttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling().setDefaultHeaders(headers)
+				.setSSLSocketFactory(connectionFactory).build();
 
-            String postArgs = "grant_type=client_credentials";
-            
-            HttpEntity<String> requestEntity = new HttpEntity<String>(postArgs,headers);
-            String uri = "https://" + host + ":8443/oauth/token";
-            log.info("uri for auth token:" + uri);
-            UaaToken response = restTemplate.postForObject(uri, requestEntity, UaaToken.class);
+		RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+		restTemplate.getInterceptors().addAll(interceptors);
 
-            log.info("Uaa token:" + response);
-    		return response.getAccess_token();
-    }
-    
-	public static String encodePassword(String username, String passowrd){
+		return restTemplate;
+	}
+
+	private static String getBoshDirectorUaaToken(String host, String directorName, String password)
+			throws GeneralSecurityException {
+
+		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).useTLS()
+				.build();
+
+		SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext,
+				new AllowAllHostnameVerifier());
+
+		HttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling()
+				.setSSLSocketFactory(connectionFactory).build();
+		RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		String base64Passowrd = encodePassword(directorName, password);
+		log.info("base64Passowrd:" + base64Passowrd);
+		headers.add("Authorization", "Basic " + base64Passowrd);
+		headers.add("Content-Type", "application/x-www-form-urlencoded");
+
+		String postArgs = "grant_type=client_credentials";
+
+		HttpEntity<String> requestEntity = new HttpEntity<String>(postArgs, headers);
+		String uri = "https://" + host + ":8443/oauth/token";
+		log.info("uri for auth token:" + uri);
+		UaaToken response = restTemplate.postForObject(uri, requestEntity, UaaToken.class);
+
+		log.info("Uaa token:" + response);
+		return response.getAccess_token();
+	}
+
+	public static String encodePassword(String username, String passowrd) {
 		String plainCreds = username + ":" + passowrd;
 		byte[] plainCredsBytes = plainCreds.getBytes();
 		byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
 		return new String(base64CredsBytes);
 	}
-
 
 }
