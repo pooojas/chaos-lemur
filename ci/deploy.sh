58a4eae068ec7c9cf3b2e54f11a18f1762ba350c
poojsharma <poojsharma2stubhub.com>
2018-08-14 14:07:57 -0700

    fix script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,4 +5,5 @@ set -e -u
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
 cd chaos-lemur
-./mvnw -q -Dmaven.test.skip=true deploy
+#./mvnw -q -Dmaven.test.skip=true deploy
+./mvnw package
